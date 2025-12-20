b04ce618aecc27c4bbf2f7b439dfd0f7c4141d20
Nabinur Islam Roni <nabinuri80@gmail.com>
2025-12-20 14:51:47 +0600

    Update environments.ts

--- PSBSFrontend/src/environments/environments.ts
+++ PSBSFrontend/src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:7272/api/',
-  uploadFileUrl: 'D:/project/SportsClient/src/assets/images/uploadedImages'
+  // uploadFileUrl: 'D:/project/SportsClient/src/assets/images/uploadedImages'
 };
