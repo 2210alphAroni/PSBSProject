7871947dc0ac11c3a04a4d48660af3a6891eacb9
Nabinur Islam Roni <nabinuri80@gmail.com>
2025-12-30 10:51:31 +0600

    creating wedding page

--- PSBSFrontend/src/Page/Photography/wedding/wedding.ts
+++ PSBSFrontend/src/Page/Photography/wedding/wedding.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-wedding',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './wedding.html',
   styleUrl: './wedding.css',
 })
 export class Wedding {
+weddingImages: any;
 
 }
