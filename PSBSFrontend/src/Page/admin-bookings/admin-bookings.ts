665c2c18cfd553e0b5094178b4131a7447c67fc2
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-01-03 00:18:34 +0600

    data get from bookings inside admin-bookings which is inside admin-dashboard

--- PSBSFrontend/src/Page/admin-bookings/admin-bookings.ts
+++ PSBSFrontend/src/Page/admin-bookings/admin-bookings.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-bookings',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './admin-bookings.html',
   styleUrl: './admin-bookings.css',
 })
-export class AdminBookings {
+export class AdminBookings implements OnInit {
 
+  bookings: any[] = [];
+  loading = true;
+  error = '';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.loadBookings();
+  }
+
+  loadBookings(): void {
+    this.http.get<any[]>('https://localhost:7272/api/Bookings')
+      .subscribe({
+        next: res => {
+          console.log('Bookings:', res); // ✅ debug
+          this.bookings = res;
+          this.loading = false;
+        },
+        error: () => {
+          this.error = 'Failed to load bookings';
+          this.loading = false;
+        }
+      });
+  }
 }
