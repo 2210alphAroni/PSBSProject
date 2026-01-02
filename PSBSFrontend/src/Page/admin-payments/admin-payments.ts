58bb9376754a22555bde1ba9e838b65a14379262
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-01-03 00:33:24 +0600

    build and update payments inside admin-dashboard data get from bookings table

--- PSBSFrontend/src/Page/admin-payments/admin-payments.ts
+++ PSBSFrontend/src/Page/admin-payments/admin-payments.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-payments',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './admin-payments.html',
   styleUrl: './admin-payments.css',
 })
-export class AdminPayments {
+export class AdminPayments implements OnInit {
+  payments: any[] = [];
+  loading = true;
+  error = '';
 
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.loadPayments();
+  }
+  loadPayments(): void {
+    this.http.get<any[]>('https://localhost:7272/api/Bookings')
+      .subscribe({
+        next: res => {
+          console.log('Payments from Bookings:', res); // ✅ debug
+          this.payments = res;
+          this.loading = false;
+          this.cdr.detectChanges();
+        },
+        error: () => {
+          this.error = 'Failed to load payments from bookings'; 
+          this.loading = false;
+        }
+      });
+  }
 }
