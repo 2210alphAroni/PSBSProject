d426ca594cee1094bb203d471d58e21b0cbe9aa3
Nabinur Islam Roni <nabinuri80@gmail.com>
2026-01-02 18:42:22 +0600

    scroll up arrow button build

--- PSBSFrontend/src/app/app.ts
+++ PSBSFrontend/src/app/app.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   template: `<router-outlet></router-outlet>`,
+//   styleUrl: './app.css'
+// })
+// export class App {}
+
+
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './shared/header/header';
+import { Footer } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet></router-outlet>`,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    Footer
+  ],
+  templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+
+  showScrollTop = false;
+
+  @HostListener('window:scroll', [])
+  onScroll() {
+    this.showScrollTop = window.scrollY > 150;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+}
